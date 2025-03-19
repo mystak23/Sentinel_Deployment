711359504e5bb95b93815047b04aa7f6e2705941
Matěj Hrabálek <matej.hrabalek@seyfor.com>
2025-03-19 19:58:38 +0100

    Git

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2Fazuredeploy.json/createUIDefinitionUri/https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2FcreateUiDefinition.json)
+
--- mac-deploy.sh
+++ mac-deploy.sh
@@ -4,8 +4,8 @@
 read -p "Zadej Tenant ID zákazníka: " tenant_id
 
 # URL k Azure Template a UI Definition
-template_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2Fazuredeploy.json"
-ui_definition_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2FcreateUiDefinition.json"
+template_url="https%3A%2F%2Fmystak23.github.io%2Fmystak23%2FSentinel_Deployment%2Fazuredeploy.json"
+ui_definition_url="https%3A%2F%2Fmystak23.github.iom%2Fmystak23%2FSentinel_Deployment%2FcreateUiDefinition.json"
 
 # Sestavení finální URL
 deploy_url="https://portal.azure.com/#create/Microsoft.Template/uri/$template_url/createUIDefinitionUri/$ui_definition_url?tenantId=$tenant_id"
