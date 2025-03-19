f342a858c622eb3a9327d485ce8d9bbaab5630be
Matěj Hrabálek <matej.hrabalek@seyfor.com>
2025-03-19 19:57:12 +0100

    Git

--- windows-deploy.ps1
+++ windows-deploy.ps1
@@ -2,8 +2,8 @@
 $tenant_id = Read-Host "Zadej Tenant ID zákazníka"
 
 # URL k Azure Template a UI Definition
-$template_url = "https%3A%2F%2Fstoredevops123.blob.core.windows.net%2Fdevops%2Fazuredeploy.json"
-$ui_definition_url = "https%3A%2F%2Fstoredevops123.blob.core.windows.net%2Fdevops%2FcreateUiDefinition.json"
+template_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2Fazuredeploy.json"
+ui_definition_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2FcreateUiDefinition.json"
 
 # Sestavení finální URL
 $deploy_url = "https://portal.azure.com/#create/Microsoft.Template/uri/$template_url/createUIDefinitionUri/$ui_definition_url?tenantId=$tenant_id"
