f24c4dce7cf8dca1accbedb020b357ee53eb679d
Matěj Hrabálek <matej.hrabalek@seyfor.com>
2025-03-24 14:47:49 +0100

    Content hub & data connectors edited

--- mac-deploy.sh
+++ mac-deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Zeptá se uživatele na Tenant ID zákazníka
-read -p "Zadej Tenant ID zákazníka: " tenant_id
+# read -p "Zadej Tenant ID zákazníka: " tenant_id
 
 # URL k Azure Template a UI Definition
 template_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2Fazuredeploy.json"
 ui_definition_url="https%3A%2F%2Fraw.githubusercontent.com%2Fmystak23%2FSentinel_Deployment%2Fmain%2FcreateUiDefinition.json"
+tenant_id="fec3a0fa-64ae-446f-a3a6-2b0eeae14c73"
 
 # Sestavení finální URL
 deploy_url="https://portal.azure.com/#create/Microsoft.Template/uri/$template_url/createUIDefinitionUri/$ui_definition_url?tenantId=$tenant_id"
